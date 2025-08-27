458f30d8fb0b861530b3f4fd4e869ad2f40b1dd1
Amir Akbarzadeh <amir.akbarzadh3@gmail.com>
2025-08-27 13:15:55 +0330

    update plugin data

--- index.php
+++ index.php
@@ -1,9 +1,16 @@
 <?php
 /*
-Plugin Name: پرداخت یاری ایران پی مکس - ووکامرس
-Version: 0.1.1
-Author: IranPaymex
-Description:  افزونه پرداخت آنلاین ایران پی مکس برای فروشگاه ساز ووکامرس
-Plugin URI: https://iranpaymex.com
+    Plugin Name: افزونه پرداخت امن و سریع ایران پی مکس برای ووکامرس
+    Version: 5.0.16
+    Description: افزونه درگاه پرداخت امن و سریع ایران پی مکس برای فروشگاه ساز ووکامرس
+    Plugin URI: https://iranpaymex.com/
+    Author: Amir Akbarzadeh
+    Author URI: https://iranpaymex.com/
+    WC requires at least: 3.0
+    WC tested up to: 9.4.1
+    Requires at least: 5.8
+    Tested up to: 6.7.1
+    License: GPLv3 or later
+    License URI: https://www.gnu.org/licenses/gpl-3.0.en.html
 */
 include_once("class-wc-gateway-iranpaymex.php");
